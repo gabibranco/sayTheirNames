2c1c9095018d89913f99f1de3d75737464833bb4
gabibranco <gabi.branco@gmail.com>
2020-06-05 09:04:52 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,59 @@
+let sayTheirNames = ['João Pedro Mattos',
+                     'Miguel Otávio Santana da Silva',
+                     'Marielle Franco',
+                     'Lucas Custódio dos Santos',
+                     'Carlos Magno de Oliveira Nascimento',
+                     'Carlos Alberto da Silva Ferreira',
+                     'Everson Gonçalves Silote',
+                     'Herinaldo Vinicius de Santana',
+                     'Alan de Souza Lima',
+                     'Douglas Rodrigues',
+                     'Matheus Santos de Morais',
+                     'Ágatha Félix',
+                     'Jean Rodrigo da Silva Aldrovande',
+                     'Fabio dos Santos Vieira',
+                     'Roberto de Souza',
+                     'Carlos Eduardo da Silva Souza',
+                     'Cleiton Corrêa de Souza',
+                     'Wesley Castro',
+                     'Wilton Esteves Domingos Junior',
+                     'Evaldo dos Santos Rosa',
+                     'Matheus Oliveira',
+                     'Evaldo Rosa dos Santos',
+                     'Marcus Vinicius',
+                     'Emilly Caetano da Costa',
+                     'Wilton Estever Domingos Jr',
+                     'Wesley Castro Rodrigues',
+                     'Cleiton Corrêa de Souza',
+                     'Carlos Eduardo Silva Souza',
+                     'Roberto Silva de Souza',
+                     'Pedro Gonzaga',
+                     'Claudia Ferreira',
+                     'Amarildo Dias de Souza',
+                     'Ketellen Gomes',
+                     'Kauã Rozario',
+                     'Rodrigo Alexandre da Silva Serrano',
+                     'Eduardo de Jesus Ferreira',
+                     'Luana Barbosa',
+                     'Rodrigo Alexandre da Silva Serrano',
+                     'Kelvin Gomes Cavalcante',
+                     'Marcos Vinícius Dos Santos',
+                     'Davi Fiuza',
+                     'Igor Mendes',
+                     'Lucas de Azevedo Albino',
+                     'Roberto Campos da Silva',
+                     'Flávio Lucas Azevedo'
+                    ]
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  textFont('Ultra');
+}
+
+function draw() {
+  let name = random(sayTheirNames);
+  text(name, random(0,windowWidth), random(0,windowHeight));
+}
